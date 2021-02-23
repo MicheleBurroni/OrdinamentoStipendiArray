b66eb2f979c2c54167aa41b9cb4bd8279a581d07
michiburro14@gmail.com <michiburro14@gmail.com>
2021-02-23 22:27:35 +0100

    Add project files.

---
+++ OrdinamentoStipendiMaxMin/Gestione.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Operazioni
+{
+    public class Gestione
+    {
+        public static void RiempimentoArray(double[] array)
+        {
+ 
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Inserire gli stipendi in posizione {i}");
+                
+                array[i] = int.Parse(Console.ReadLine());
+            }
+        }
+            public static double Max(double[] array)
+        {
+            double max = double.MinValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+            }
+            return max;
+        }
+        public static double Min(double[] array)
+        {
+            double min = double.MaxValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+            }
+            return min;
+        }
+        public static void BubbleSort(double[] array)
+        {
+            int x, j;
+            double temp;
+            for (x = 0; x < array.Length - x; x++)
+            {
+                for (j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        public static void StampaArray(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+        }
+    }
+}
---
+++ OrdinamentoStipendiMinMax/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Operazioni;
+     
+namespace OrdinamentoStipendiMinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci gli stipendi:");
+            int n = int.Parse(Console.ReadLine());
+            double[] array = new double[n];
+            Gestione.RiempimentoArray(array);
+            Gestione.BubbleSort(array);
+            Console.WriteLine($"Stipendio minimo: {array[0]}.");
+            Console.WriteLine($"Stipendio massimo:{array[array.Length - 1]}.");
+            Gestione.StampaArray(array);
+        }
+    }
+}
